9d1b243e9b348abc0dd351752f126a802c35670e
Joseph Keinan <yk3823@gmail.com>
2023-05-24 18:00:30 +0300

    adding code to backend side

---
+++ viteproject01/src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
