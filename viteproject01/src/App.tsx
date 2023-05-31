e90a45989c6498d2d50d416d5a22297fd28e3aed
Joseph Keinan <yk3823@gmail.com>
2023-05-31 18:20:43 +0300

    react 31.05

--- viteproject01/src/App.tsx
+++ viteproject01/src/App.tsx
@@ -1,9 +1,20 @@
 import ListGroup from "./components/ListGroup";
+import "./App.css";
 
-function App(){
-  
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelctItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelctItem}
+      />
+    </div>
+  );
 }
-
 
 export default App;
--- viteproject01/src/components/ListGroup.tsx
+++ viteproject01/src/components/ListGroup.tsx
@@ -1,15 +1,47 @@
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+import { useState } from "react";
+import "./ListGroup.css";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+interface ListItemProps {
+  active: boolean;
+}
+const ListItem = styled.li<ListItemProps>`
+  padding: 10px 0;
+  background: ${(props) => (props.active ? "pink" : "none")};
+`;
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
-      <h1>List</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <List>
+        {items.map((item, index) => (
+          <ListItem
+            active={index === selectedIndex}
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
+
 export default ListGroup;
